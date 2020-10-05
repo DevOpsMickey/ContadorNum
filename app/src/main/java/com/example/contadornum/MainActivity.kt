823d507fdcc1f4a121d05c66b1df47d6f7182301
Mickeyfeick <68301522+Mickeyfeick@users.noreply.github.com>
2020-10-04 20:02:54 -0500

    App contador

--- app/src/main/java/com/example/contadornum/MainActivity.kt
+++ app/src/main/java/com/example/contadornum/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.contadornum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         btnRestar.setOnClickListener {
             contadorNum--
             txtNum.text = contadorNum.toString()
+        }
+
+        btnAbrirVentana.setOnClickListener{
+            val abrirVentana = Intent(this, Ventana_Descripcion::class.java);
+            startActivity(abrirVentana)
         }
 
     }
---
+++ app/src/main/java/com/example/contadornum/Ventana_Descripcion.kt
@@ -0,0 +1,18 @@
+package com.example.contadornum
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_ventana__descripcion.*
+
+class Ventana_Descripcion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ventana__descripcion)
+
+        btnRegresar.setOnClickListener{
+            val atras = Intent(this, MainActivity::class.java);
+            startActivity(atras)
+        }
+    }
+}
